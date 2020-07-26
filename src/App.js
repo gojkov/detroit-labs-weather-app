2f260ebdf41cafe82680f04f40069b8bf416231e
Steve Gojkov <stevegojkov@gmail.com>
2020-07-26 17:26:34 -0400

    add getLocation function to retrieve location from HTML5 API, if disabled uses added getIP function

--- src/App.js
+++ src/App.js
@@ -5,7 +5,31 @@ import './App.css';
 
 export default function App() {
   const API_KEY = `bb10b69a4c319571b3ff6623ff802dea`;
-  
+  const [globalStore, setGlobalStore] = useContext(GlobalStoreContext);
+
+  const getLocation = () => {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+
+      },
+      err => {
+        console.warn(`ERROR(${err.code}): ${err.message}`);
+      }
+    );
+  };
+
+  const getIP = async () => {
+    const IP_URL_HOME = 'https://ipapi.co/json/';
+    const json = await axios.get(IP_URL_HOME);
+    setGlobalStore({
+      ...globalStore,
+      latitude: json.data.latitude,
+      longitude: json.data.longitude,
+      city: json.data.city
+    });
+  };
+
+
   return (
     <div className="App">
       
