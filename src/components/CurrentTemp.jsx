bb0c2e9640fd0db493986e37439019dce757ce64
Steve Gojkov <stevegojkov@gmail.com>
2020-07-28 13:14:53 -0400

    fixed CurrentTemp edge cases of long city names

--- src/components/CurrentTemp.jsx
+++ src/components/CurrentTemp.jsx
@@ -4,18 +4,22 @@ import '../styles/CurrentTemp.scss';
 
 const CurrentTemp = () => {
     const [globalStore] = useContext(GlobalStoreContext);
+    const testCity = 'Grosse Pointe Farms';
 
     return (
-        <div className="current-temp-card">
-            <h1>{globalStore.city}</h1>
+        <div
+            className={
+                testCity.length <= 12 ? `current-temp-card` : `long-name-card`
+            }
+        >
+            <h1>{testCity}</h1>
             <img
                 src={`http://openweathermap.org/img/wn/${globalStore.icon}@2x.png`}
                 alt="weather icon"
             />
             <h1>{globalStore.currentTemp}&deg;</h1>
             <h5>
-                It is currently {globalStore.currentTemp}&deg; in{' '}
-                {globalStore.city}.
+                It is currently {globalStore.currentTemp}&deg; in {testCity}.
             </h5>
         </div>
     );
