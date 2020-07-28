e0d967d96606bda3f54e5010934954bba6bd81ba
Steve Gojkov <stevegojkov@gmail.com>
2020-07-28 13:17:32 -0400

    replaced temporary long name with correct name variable

--- src/components/CurrentTemp.jsx
+++ src/components/CurrentTemp.jsx
@@ -4,22 +4,24 @@ import '../styles/CurrentTemp.scss';
 
 const CurrentTemp = () => {
     const [globalStore] = useContext(GlobalStoreContext);
-    const testCity = 'Grosse Pointe Farms';
 
     return (
         <div
             className={
-                testCity.length <= 12 ? `current-temp-card` : `long-name-card`
+                globalStore.city.length <= 12
+                    ? `current-temp-card`
+                    : `long-name-card`
             }
         >
-            <h1>{testCity}</h1>
+            <h1>{globalStore.city}</h1>
             <img
                 src={`http://openweathermap.org/img/wn/${globalStore.icon}@2x.png`}
                 alt="weather icon"
             />
             <h1>{globalStore.currentTemp}&deg;</h1>
             <h5>
-                It is currently {globalStore.currentTemp}&deg; in {testCity}.
+                It is currently {globalStore.currentTemp}&deg; in{' '}
+                {globalStore.city}.
             </h5>
         </div>
     );
